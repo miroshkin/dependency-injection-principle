52883907ba1cb6becabda4c159b80bf278bc1641
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-22 17:45:06 +0300

    Added simple DIP example

--- DependencyInjectionPrinciple/Program.cs
+++ DependencyInjectionPrinciple/Program.cs
@@ -10,6 +10,49 @@ namespace DependencyInjectionPrinciple
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, DIP!");
+           
+         
+        }
+
+        public class CustomerBusinessLogic
+        {
+            private ICustomerDataAccess _custDataAccess;
+
+            public CustomerBusinessLogic()
+            {
+            }
+
+            public string GetCustomerName(int id)
+            {
+                _custDataAccess = DataAccessFactory.GetDataAccessObj();
+
+                return _custDataAccess.GetCustomerName(id);
+            }
+        }
+
+        public class DataAccessFactory
+        {
+            public static ICustomerDataAccess GetDataAccessObj() 
+            {
+                return new CustomerDataAccess();
+            }
+        }
+
+        public interface ICustomerDataAccess
+        {
+            string GetCustomerName(int id);
+        }
+
+        public class CustomerDataAccess: ICustomerDataAccess
+        {
+            public CustomerDataAccess()
+            {
+            }
+
+            public string GetCustomerName(int id) {
+                return "Dummy Customer Name";        
+            }
         }
     }
 }
