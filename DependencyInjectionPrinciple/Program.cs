edefeee3a6a54574b234eb9378c14660d3ba25fa
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-22 18:17:07 +0300

    Added naming registering

--- DependencyInjectionPrinciple/Program.cs
+++ DependencyInjectionPrinciple/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
 
 namespace DependencyInjectionPrinciple
 {
@@ -10,8 +11,13 @@ namespace DependencyInjectionPrinciple
     {
         static void Main(string[] args)
         {
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<ICar, BMW>();
+            container.RegisterType<ICar, Audi>("LuxuryCar");
+            
             Console.WriteLine("Hello, DIP!");
-           
+            Driver driver = container.Resolve<Driver>(); 
+            driver.RunCar();
          
         }
 
@@ -95,6 +101,56 @@ namespace DependencyInjectionPrinciple
 
             public string GetCustomerName(int id) {
                 return _customerBL.GetCustomerName(id);
+            }
+        }
+
+        public interface ICar
+        {
+            int Run();
+        }
+
+        public class BMW : ICar
+        {
+            private int _miles = 0;
+
+            public int Run()
+            {
+                return ++_miles;
+            }
+        }
+
+        public class Ford : ICar
+        {
+            private int _miles = 0;
+
+            public int Run()
+            {
+                return ++_miles;
+            }
+        }
+
+        public class Audi : ICar
+        {
+            private int _miles = 0;
+
+            public int Run()
+            {
+                return ++_miles;
+            }
+
+        }
+        public class Driver
+        {
+            private ICar _car = null;
+
+            public Driver(ICar car)
+            {
+                _car = car;
+            }
+
+            public void RunCar()
+            {
+                Console.WriteLine("Running {0} - {1} mile ", _car.GetType().Name, _car.Run());
             }
         }
     }
